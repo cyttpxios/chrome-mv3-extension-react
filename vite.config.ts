73c566aac02e13a4f035e6bdee6b9b5ee197faa7
塵_ <499436550@qq.com>
2025-04-27 19:47:36 +0800

    chore: add build config for production

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { crx } from '@crxjs/vite-plugin'
 import manifest from './manifest.json'
-
+import path from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
@@ -20,6 +20,24 @@ export default defineConfig(({ command, mode }) => {
       'import.meta.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL),
     },
     plugins: [react(), crx({ manifest }),],
+    build: {
+      outDir: path.resolve(__dirname, 'dist'),
+      minify: isDev ? false : 'terser',
+      terserOptions: {
+        compress: {
+          drop_console: true,
+          drop_debugger: true,
+          passes: 5, 
+        },
+        format: {
+          comments: false,
+        },
+        toplevel: true,
+        keep_classnames: true,
+        keep_fnames: true,
+      },
+
+    },
   }
 })
 
